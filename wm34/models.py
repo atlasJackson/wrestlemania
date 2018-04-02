4692bccf6389317a2b2f17007a9a2b9ce38f4f77
Gordon Daffurn <le.g.funkadelic@googlemail.com>
2018-04-02 19:09:17 +0100

    Started models.

--- wm34/models.py
+++ wm34/models.py
@@ -14,3 +14,50 @@ class UserProfile(models.Model):
     #Override the __unicode__() method to return out something meaningful!
     def __str__(self):
         return self.user.username
+
+
+class Event(models.Model):    
+    name = models.CharField(max_length=128)
+    date = models.DateTimeField(default=None)
+    user = models.ManyToManyField(User, through="UserEvent")
+
+
+class UserEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    points = IntegerField(default=0)
+
+
+class Match(models.Model):
+
+    event = models.ForeignKey(Event)
+
+    # Game types take the following values in order to reference integer field:
+    SINGLE = 1
+    TAG = 2
+    TRIPLE = 3
+    FOUR = 4
+    TRIPLE_TAG = -5
+    ROYALE = 6
+    LADDER = 7
+    TABLE = 8
+
+    MATCH_CHOICES = (
+        (SINGLE, "Singles"),
+        (TAG, "Tag Team"),
+        (TRIPLE< "Triple Threat"),
+        (FOUR, "Fatal Four-way"),
+        (TRIPLE_TAG, "Triple Threat Tag Team"),
+        (ROYALE, "Battle Royale"),
+        (LADDER, "Ladder"),
+        (TABLE, "Tables"),
+    )
+
+    elimination = models.BooleanField(default=False)
+    match_type = models.IntegerField(choices=MATCH_CHOICES, default=SINGLE)
+
+
+class Wrestler(models.Model):
+    name = models.CharField(max_length=128)
+    match = models.ManyToManyField(Match)
+
