03ada5185c1cef6fa91087169de726814a083e09
endless-summer <lylesimpson89@gmail.com>
2018-04-05 10:59:11 +0100

    Match type in the data dictionary set to Match model constants, and a test method added for basic verification of the database after the script runs

--- populate.py
+++ populate.py
@@ -23,41 +23,41 @@ def populate():
         "matches": {
             "universal_championship": {
                 "wrestlers": ["Brock Lesnar", "Roman Reigns"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "wwe_championship": {
                 "wrestlers": ["AJ Styles", "Shinsuke Nakamura"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "tag_match": {
                 "wrestlers": ["Ronda Rousey", "Kurt Angle", "Stephanie McMahon", "Triple H"],
                 "teams": {"Ronda Rousey & Kurt Angle": ["Ronda Rousey", "Kurt Angle"], 
                           "Stephanie McMahon & Triple H": ["Stephanie McMahon", "Triple H"]
                          },
-                "match_type": 2
+                "match_type": Match.TAG
             },
             "tag_match2": {
                 "wrestlers": ["Shane McMahon", "Daniel Bryan", "Kevin Owens", "Sami Zayn"],
                 "teams": {"Shane McMahon & Daniel Bryan": ["Shane McMahon", "Daniel Bryan"], 
                           "Kevin Owens & Sami Zayn": ["Kevin Owens", "Sami Zayn"]
                          },
-                "match_type": 2
+                "match_type": Match.TAG
             },
             "smackdown_womens_championship": {
                 "wrestlers": ["Charlotte Flair", "Asuka"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "raw_womens_championship": {
                 "wrestlers": ["Alexa Bliss", "Nia Jax"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "intercontinental_championship": {
                 "wrestlers": ["The Miz", "Seth Rollins", "Finn Balor"],
-                "match_type": 3
+                "match_type": Match.TRIPLE
             },
             "united_states_championship": {
                 "wrestlers": ["Randy Orton", "Bobby Roode", "Jinder Mahal", "Rusev"],
-                "match_type": 4
+                "match_type": Match.FOUR
             },
             "smackdown_tagteam_championship": {
                 "wrestlers": ["Jey Uso", "Jimmy Uso", "Big E", "Kofi Kingston", "Xavier Woods", "Luke Harper", "Eric Rowan"],
@@ -65,67 +65,83 @@ def populate():
                           "The New Day": ["Big E", "Kofi Kingston", "Xavier Woods"],
                           "The Bludgeon Brothers": ["Luke Harper", "Eric Rowan"]
                           },
-                "match_type": 5
+                "match_type": Match.TRIPLE_TAG
             },
             "raw_tagteam_championship": {
                 "wrestlers": ["Cesaro", "Sheamus", "Braun Strowman"],
                 "teams": {"The Bar": ["Cesaro", "Sheamus"], 
                           "Braun Strowman & TBD": ["Braun Strowman",]
                           },
-                "match_type": 2
+                "match_type": Match.TAG
             },
             "cruiserweight_championship": {
                 "wrestlers": ["Cedric Alexander", "Mustafa Ali"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "taker_cena": {
                 "wrestlers": ["Undertaker", "John Cena"],
-                "match_type": 1
+                "match_type": Match.SINGLE
             },
             "womens_battle_royal": {
                 "wrestlers": ["Bayley", "Sasha Banks","Ruby Riott","Sarah Logan","Liv Morgan","Becky Lynch","Naomi","Natalya","Lana","Mickie James","Mandy Rose","Sonya Deville","Carmella"], 
-                "match_type": 6,
+                "match_type": Match.ROYALE,
                 "name": "WrestleMania Women's Battle Royal"
             },
             "andre_the_giant_battle_royal": {
                 "wrestlers": ["Dolph Ziggler","Tyler Breeze","Fandango","Woken Matt Hardy","Baron Corbin",
                             "Mojo Rawley","Tye Dillinger","Dash Wilder","Scott Dawson","Goldust","Heath Slater",
                             "Rhyno","Zack Ryder","Primo Colon"],
-                "match_type": 6,
+                "match_type": Match.ROYALE,
                 "name": "Andre the Giant Battle Royal"
             }
+        },
+
+        "users": {
+            ####
         }
     }
+
+    # Test variables for comparing against the number of database results for each model after the populate script has run
+    MATCH_COUNT = len(data["matches"])
+    WRESTLER_COUNT = 0
+    TEAM_COUNT = 0
+    for keys in data["matches"].values():
+        WRESTLER_COUNT += len(keys["wrestlers"])
+        if 'teams' in keys:
+            TEAM_COUNT += len(keys["teams"])
 
     # Create event:
     wm34 = insert_event(data["name"], data["date"])
     
+    # Create and link the other model entities
     for match_name, match_data in data["matches"].items():
-        try:
-            this_match = insert_match(wm34, match_data["match_type"], match_data["name"])
-        except KeyError:
-            this_match = insert_match(wm34, match_data["match_type"])
-        for wrestler in match_data["wrestlers"]:
-            w = insert_wrestler(wrestler)
-            # Create link between wrester and match
-            link_wrestler_and_match(w, this_match)
-        try:
-            for team, wrestlers in match_data["teams"].items():
-                t = insert_team(team)
-                # Create link between team and wrestlers
-                for w in wrestlers:
-                    link_wrestler_and_team(w, t)
-                # Create link between team and match
-                link_team_and_match(t, this_match)
-        except KeyError:
-            pass
-        
+        if Match.objects.count() < MATCH_COUNT:
+            try:
+                this_match = insert_match(wm34, match_data["match_type"], match_data["name"])
+            except KeyError:
+                this_match = insert_match(wm34, match_data["match_type"])
+            for wrestler in match_data["wrestlers"]:
+                w = insert_wrestler(wrestler)
+                # Create link between wrester and match
+                link_wrestler_and_match(w, this_match)
+            try:
+                for team, wrestlers in match_data["teams"].items():
+                    t = insert_team(team)
+                    # Create link between team and wrestlers
+                    for w in wrestlers:
+                        link_wrestler_and_team(w, t)
+                    # Create link between team and match
+                    link_team_and_match(t, this_match)
+            except KeyError:
+                pass
+    
+    # Basic test that checks the correct number of entities have been entered to the DB
+    test(MATCH_COUNT, WRESTLER_COUNT, TEAM_COUNT)       
 
 
+# Database insert functions
 def insert_event(name, date):
-    print(name,date)
     e = Event.objects.get_or_create(name=name, date=date)[0]
-    e.date = date
     e.save()
     return e
 
@@ -152,6 +168,16 @@ def link_wrestler_and_match(wrestler,match):
 
 def link_team_and_match(team,match):
     match.team.add(team)
+
+def test(match_count, wrestler_count, team_count):
+    m = Match.objects.count() == match_count
+    w = Wrestler.objects.count() == wrestler_count
+    t = Team.objects.count() == team_count
+
+    if m and w and t:
+        print("Data entered successfully to the database")
+    else:
+        print("Something went wrong!")
 
 if __name__ == '__main__':
     populate()
